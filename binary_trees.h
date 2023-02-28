0aff03409603a73799dc6abaf3413b17479c5860
eyasucC <eyasuchaka129@gmail.com>
2023-02-27 21:54:07 -0800

    binary_tree-2

--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,6 @@ typedef struct levelorder_queue_s
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 
